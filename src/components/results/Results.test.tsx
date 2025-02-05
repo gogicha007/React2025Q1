fdd53caeb1d7a39cd3f910f7482e435738b8fa06
gogicha007 <gogicha@gmail.com>
2025-02-05 16:56:54 +0400

    feat: test card comp renders the relevant data

--- src/components/results/Results.test.tsx
+++ src/components/results/Results.test.tsx
@@ -3,12 +3,13 @@ import { enableFetchMocks } from 'jest-fetch-mock';
 import '@testing-library/jest-dom';
 import { MemoryRouter, RouterProvider, createMemoryRouter } from 'react-router';
 import userEvent from '@testing-library/user-event';
+import HomePage from '../../pages/home/home';
 import Results from './results';
+import { Card } from '../card/card';
+import { Details } from '../details/details';
 import { IFResponse } from '../../types/interface';
 import { mockData } from '../../testing/mocks/mock_data';
 import { mockFetch } from '../../testing/mocks/mock-fetch';
-import HomePage from '../../pages/home/home';
-import { Details } from '../details/details';
 enableFetchMocks();
 
 describe('rs-app-router', () => {
@@ -16,7 +17,7 @@ describe('rs-app-router', () => {
     jest.clearAllMocks();
   });
 
-  test('check number of cards listed', async () => {
+  test('Verify that the component renders the specified number of cards', async () => {
     window.fetch = mockFetch(mockData as IFResponse);
     render(
       <MemoryRouter initialEntries={['?page=1&status=Alive']}>
@@ -30,7 +31,7 @@ describe('rs-app-router', () => {
     });
   });
 
-  test('check if no results provided', async () => {
+  test('Check that an appropriate message is displayed if no cards are present', async () => {
     window.fetch = mockFetch(null);
     render(
       <MemoryRouter>
@@ -43,20 +44,14 @@ describe('rs-app-router', () => {
     });
   });
 
-  test('check if card link contains proper href', async () => {
+  test('Ensure that the card component renders the relevant card data', async () => {
     window.fetch = mockFetch(mockData as IFResponse);
-    render(
-      <MemoryRouter initialEntries={['?page=1&status=Alive']}>
-        <Results loader={true} />
-      </MemoryRouter>
-    );
-    await waitFor(() => {
-      const listLinks: HTMLAnchorElement[] = screen.getAllByRole('link');
-      expect(listLinks[1].href).toContain('/2');
-    });
+    render(<Card {...mockData.results[0]} />);
+    const name = screen.getByText(mockData.results[0].name);
+    expect(name.textContent).toEqual('card 1');
   });
 
-  test('check if click on card navigates to details', async () => {
+  test('Validate that clicking on a card opens a detailed card component', async () => {
     window.fetch = mockFetch(mockData as IFResponse);
     const router = createMemoryRouter(
       [
@@ -74,5 +69,18 @@ describe('rs-app-router', () => {
     const card1 = screen.getByRole('link', { name: /card 1 alive/i });
     await userEvent.click(card1);
     expect(screen.getByRole('button', { name: 'Close details' }));
+  });
+});
+
+test('check if card link contains proper href', async () => {
+  window.fetch = mockFetch(mockData as IFResponse);
+  render(
+    <MemoryRouter initialEntries={['?page=1&status=Alive']}>
+      <Results loader={true} />
+    </MemoryRouter>
+  );
+  await waitFor(() => {
+    const listLinks: HTMLAnchorElement[] = screen.getAllByRole('link');
+    expect(listLinks[1].href).toContain('/2');
   });
 });
--- src/types/interface.ts
+++ src/types/interface.ts
@@ -14,14 +14,14 @@ export interface IFCharacter {
   id: number;
   name: string;
   status: string;
-  species: string;
-  gender: string;
-  image: string;
-  origin: {
-    name: string;
+  species?: string;
+  gender?: string;
+  image?: string;
+  origin?: {
+    name?: string;
   };
-  location: {
-    name: string;
+  location?: {
+    name?: string;
   };
 }
 
