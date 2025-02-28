ac182a6dcf4b19f4c58ecf949b5181a0e1b398dd
gogicha007 <gogicha@gmail.com>
2025-02-28 12:56:29 +0400

    refactor: details side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,24 @@
-import styles from '../src/styles/Home.module.css';
+import styles from '../styles/Home.module.css';
 import SearchBar from '@/components/search/SearchBar';
 import { useCharacterFilters } from '@/hooks/useCharacterFilter';
 // import { useEffect } from 'react';
 import ThemeControls from '@/components/theme-controls/ThemeControls';
 import Loader from '@/components/loader/loader';
 import Results from '@/components/cards-list/CardList';
+import Details from '@/components/details/Details';
+import { useRouter } from 'next/router';
 import { Pagination } from '@/components/pagination/Pagination';
 import { useGetListQuery } from '@/state/features/characters/charactersApiSlice';
 import type { IQueryError } from '@/types/interface';
 
 export default function Home() {
+  const router = useRouter();
+  const { id } = router.query;
   const { page, status } = useCharacterFilters();
-  const { data, isFetching, error } = useGetListQuery(
-    {
-      page: +page,
-      status: status,
-    },
-    {
-      skip: !page,
-      refetchOnMountOrArgChange: true,
-      refetchOnFocus: true,
-      refetchOnReconnect:true,
-    }
-  );
+  const { data, isFetching, error } = useGetListQuery({
+    page: +page,
+    status: status,
+  });
 
   // useEffect(() => {
   //   console.log('Page:', page, 'Status:', status);
@@ -45,17 +41,28 @@ export default function Home() {
       </header>
       <main>
         {error && <h1>{(error as IQueryError).status}</h1>}
-        {!error && data && (
-          <div className={styles.home__main}>
-            <div
-              className={styles.home__cardlist}
-              data-testid={styles.home__cardlist}
-              onClick={() => handleListClick()}
-            >
-              <Results {...data} />
-              <div className={styles.home__devider}></div>
-              <Pagination resInfo={data.info} />
+        {!error && (
+          <div
+            className={`${styles.home_main} ${id ? styles.with_details : ''}`}
+          >
+            <div className={styles.home__cardlist_container}>
+              {data && (
+                <div
+                  className={styles.home__cardlist}
+                  data-testid={styles.home__cardlist}
+                  onClick={() => handleListClick()}
+                >
+                  <Results {...data} />
+                  <div className={styles.home__devider}></div>
+                  <Pagination resInfo={data.info} />
+                </div>
+              )}
             </div>
+            {id && (
+              <div className={styles.details_panel}>
+                <Details />
+              </div>
+            )}
           </div>
         )}
       </main>
--- src/components/cards-list/CardList.tsx
+++ src/components/cards-list/CardList.tsx
@@ -1,15 +1,16 @@
 import './card-list.css';
 import { useEffect, useState } from 'react';
-import Link from 'next/link';
 import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
 import { Card } from '../card/card';
 import type { ICharacterDetails, IResponse } from '../../types/interface';
 import type { RootState } from '../../state/store';
 import PickCards from '../../state/features/pickCards/PickCards';
 
 const Results = (data: IResponse) => {
+  const router = useRouter();
   const [detailsOpen, setDetailsOpen] = useState(false);
-
+  console.log(detailsOpen)
   const selectedCards = useSelector(
     (state: RootState) => state.selectedCards.selectedCards
   );
@@ -18,18 +19,38 @@ const Results = (data: IResponse) => {
     setDetailsOpen(false);
   }, [data]);
 
+  const handleCardClick = (
+    e: React.MouseEvent,
+    character: ICharacterDetails
+  ) => {
+    e.preventDefault();
+    router.push(
+      {
+        pathname: '/',
+        query: {
+          ...router.query,
+          id: character.id,
+        },
+      },
+      undefined,
+      { shallow: true }
+    );
+  };
+  const isSelected = (id: string | number) => {
+    return router.query.id === id.toString();
+  };
+
   return (
     <div className="results" data-testid="results">
       {data?.results?.map((obj: ICharacterDetails) => (
-        <Link
-          href={{ pathname: `${obj.id}`, search: `${location.search}` }}
+        <div
           key={obj.id}
-          onClick={() => console.log(`${detailsOpen}`)}
+          onClick={(e) => handleCardClick(e, obj)}
+          role="card"
+          className={`card-wrapper ${isSelected(obj.id) ? 'selected' : ''}`}
         >
-          <div role="card">
-            <Card {...obj} />
-          </div>
-        </Link>
+          <Card {...obj} />
+        </div>
       ))}
       <div className="results__select_controls">
         {selectedCards.length > 0 && <PickCards data={data} />}
