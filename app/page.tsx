253864b5cde8b22df64bfb624ddfbeda5b0ad571
gogicha007 <gogicha@gmail.com>
2025-03-07 09:15:35 +0400

    refactor: page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import ClientComponent from './ClientComponent';
-import type { IResponse } from 'types/interface';
+import styles from '../styles/Home.module.css';
+import SearchBar from '../components/search/SearchBar';
+import { useCharacterFilters } from 'hooks/useCharacterFilter';
+import ThemeControls from '../components/theme-controls/ThemeControls';
+import Loader from '../components/loader/loader';
+import Results from 'components/cards-list/CardList';
+import Details from 'components/details/Details';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Pagination } from '../components/pagination/Pagination';
+import { useGetListQuery } from 'state/features/characters/charactersApiSlice';
+import type { IQueryError, IResponse } from 'types/interface';
+import NotFound from '../components/not-found/404';
 
 export default function Home() {
   const [initialData, setInitialData] = useState<IResponse | null>(null);
@@ -21,11 +31,68 @@ export default function Home() {
     fetchData();
   }, [initialPage, initialStatus]);
 
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const id = searchParams ? searchParams.get('id') : null;
+  const { page, status } = useCharacterFilters();
+  const { data = initialData, isFetching, error } = useGetListQuery({
+    page: +page || initialPage,
+    status: status || initialStatus as string,
+  });
+
+  const handleListClick = () => {
+    if (id) {
+      const params = new URLSearchParams(searchParams ? searchParams.toString() : '');
+      params.delete('id');
+      router.push(`/?${params.toString()}`);
+    }
+  };
+
   if (!initialData) {
-    return <div>Loading...</div>;
+    return <Loader />;
   }
 
   return (
-    <ClientComponent initialData={initialData} initialPage={initialPage} initialStatus={initialStatus} />
+    <div className={styles.home}>
+      <header className={styles.home__top}>
+        <SearchBar />
+        <ThemeControls />
+      </header>
+      <main>
+        {error &&
+          ((error as IQueryError).status === 404 ? (
+            <NotFound />
+          ) : (
+            <h1>Error loading data</h1>
+          ))}
+        {!error && (
+          <div
+            className={`${styles.home__main} ${id ? styles.with_details : ''}`}
+          >
+            <div className={styles.home__cardlist_container}>
+              {data && (
+                <div
+                  className={styles.home__cardlist}
+                  data-testid="home__cardlist"
+                  onClick={() => handleListClick()}
+                >
+                  <Results {...data} />
+                  <div className={styles.home__devider}></div>
+                  <Pagination resInfo={data.info} />
+                </div>
+              )}
+            </div>
+            <div>
+              {id && (
+                <div className={styles.details_panel}>
+                  <Details />
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </main>
+      {isFetching && <Loader />}
+    </div>
   );
 }
