0affe660a193dfc8ceda6c90431fa1cb2b4fc37f
gogicha007 <gogicha@gmail.com>
2025-03-14 17:57:35 +0400

    chore: save the progress

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,12 @@ function App() {
     {
       element: <Layout />,
       children: [
-        { path: '/', element: <Home /> },
-        { path: '/uncontrolled_form', element: <PlainForm /> },
-        { path: '/hook_form', element: <HookForm /> },
+        { path: '/React2025Q1/forms/', element: <Home /> },
+        {
+          path: '/React2025Q1/forms/uncontrolled_form',
+          element: <PlainForm />,
+        },
+        { path: '/React2025Q1/forms//hook_form', element: <HookForm /> },
         { path: '*', element: <h1>Not Found</h1> },
       ],
     },
