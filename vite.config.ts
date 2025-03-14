bdc7c4fdcfb3fc1be8f84e170a0ec88d28c160e5
gogicha007 <gogicha@gmail.com>
2025-03-14 17:40:10 +0400

    chore: save the current

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React2025Q1/forms/',
+  base: '/React2025Q1',
 });
